8e10e352dce9a380398c114fbe5958250673b485
Matheus Forte de Melo <114398115+Melao12125@users.noreply.github.com>
2023-08-07 19:16:45 -0300

    Add files via upload

---
+++ codigo-das-banana/codigo-das-banana.ino
@@ -0,0 +1,53 @@
+#include <BleKeyboard.h>
+
+//============== Definindo teclas ======================
+String teclas[7] = {"q", "w", "e", "r", "t", "y", "u"};
+//String teclas[7] = {1, 2, 3, 4, 5, 6, 7}
+//String teclas[7] = {i, o, p, a, s, d, f}
+//======================================================
+
+//==============Configurações Iniciais===================
+bool teclaPressionada[7] = {false, false, false, false, false, false, false};   
+const int pinosBananas[7] = {4, 13, 12, 15, 14, 27, 33 };  
+BleKeyboard bleKeyboard("TecladoBanana", "Math e Thigas", 100);
+//=======================================================
+
+void setup() {
+  for (int i = 0; i < 8; i++) {
+    touchAttachInterrupt(pinosBananas[i], teclaTocada, 40);
+    pinMode(pinosBananas[i], INPUT);
+  }
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  bleKeyboard.begin();
+}
+
+void loop() {
+  if(bleKeyboard.isConnected()) {
+    Serial.println("Conectado!!!");
+  } else {
+    Serial.println("Desconectado!!!");
+  }
+
+  delay(250);
+}
+
+void teclaTocada() {
+  for (int i = 0; i < 8; i++) {
+    if (touchRead(pinosBananas[i]) < 22) {
+      if (!teclaPressionada[i]) {
+        teclaPressionada[i] = true;
+        bleKeyboard.press(teclas[i][0]); // Pressiona a tecla 
+        //Serial.println("Tecla pressionada: " + teclas[i]);
+        //setDelay()? blekey
+        digitalWrite(2, HIGH);
+      }
+    } else {
+      if (teclaPressionada[i]) {
+        teclaPressionada[i] = false;
+        bleKeyboard.release(teclas[i][0]); // Libera a tecla 
+        digitalWrite(2, LOW);
+      }
+    }
+  }
+}
