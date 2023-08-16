6dc624e1da767599522eac8018d474a78c2b6c28
Matheus Forte de Melo <114398115+Melao12125@users.noreply.github.com>
2023-08-16 14:25:33 -0300

    Update codigo-das-banana.ino

--- codigo-das-banana/codigo-das-banana.ino
+++ codigo-das-banana/codigo-das-banana.ino
@@ -1,66 +1,75 @@
-// Branch Original utilizando apenas BleKeyboard para emulação de teclas BlueTooth
+// Branch utilizando BleKeyboard para emulação de teclas BlueTooth enquanto conecta-se com WiFi
 // Escolha um dos arquivos BleKeyboard ZIP na pasta 'downloads' do repositório e o importe para seu sketch.
 
 // Aviso: Se utilzando a versão otimizada do BleKeyboard importe para o sketch a biblioteca NimBle-Arduino!
 // Não precisa incluir a biblioteca NiBle-Arduino no codigo, só basta estar presente dentro do sketch.
 
 #include <BleKeyboard.h>
+#include <WiFi.h>
 
-//============== Definindo teclas ======================
+//================Configurações de Pinagem/Wifi/Bluetooth=====================
+const char* ssid = "NomeDaRede";
+const char* password = "SenhaDaRede";
 String teclas[7] = {"q", "w", "e", "r", "t", "y", "u"};
-//String teclas[7] = {1, 2, 3, 4, 5, 6, 7} 
-//String teclas[7] = {i, o, p, a, s, d, f}
-//======================================================
-
-//==============Configurações Iniciais===================
-bool teclaPressionada[7] = {false, false, false, false, false, false, false};   
-const int pinosBananas[7] = {4, 13, 12, 15, 14, 27, 33 };  
+bool teclaPressionada[7] = {false, false, false, false, false, false, false};
+const int pinosBananas[7] = {4, 13, 12, 15, 14, 27, 33};
 BleKeyboard bleKeyboard("TecladoBanana", "Math e Thigas", 100);
-//=======================================================
 
-//=================== Setup ===========================
+//=======================Setup==============================
 void setup() {
   for (int i = 0; i < 8; i++) {
     touchAttachInterrupt(pinosBananas[i], teclaTocada, 40);
     pinMode(pinosBananas[i], INPUT);
   }
   pinMode(2, OUTPUT);
-  Serial.begin(115200);
-  bleKeyboard.begin();
-}
-//=======================================================
 
-//================= Loop Principal ======================
+  Serial.begin(115200);
+  connectToWiFi();
+  bleKeyboard.begin();
+
+//=========================Loop=========================
 void loop() {
-  if(bleKeyboard.isConnected()) {
-    Serial.println("Conectado!!!");
+  if (bleKeyboard.isConnected()) {
+    Serial.println("Teclado Conectado");
   } else {
-    Serial.println("Desconectado!!!");
+    Serial.println("Teclado Desconectado");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Conectado ao WiFi");
+  } else {
+    Serial.println("Desconectado do WiFi");
   }
   delay(250);
 }
-//=======================================================
 
-//============= Logica das Teclas/Pinos ===================
+//======================Funções========================
 void teclaTocada() {
   for (int i = 0; i < 8; i++) {
-    if (touchRead(pinosBananas[i]) < 22) { // Se a leitura do pino for menor que 22 significa que a tecla está pressionada.
-      //Logica para pressionar a tecla .
+    if (touchRead(pinosBananas[i]) < 22) {
       if (!teclaPressionada[i]) {
         teclaPressionada[i] = true;
-        bleKeyboard.press(teclas[i][0]); // Pressiona a tecla 
-        //Serial.println("Tecla pressionada: " + teclas[i]);
-        //setDelay()? blekey
+        bleKeyboard.press(teclas[i][0]);
         digitalWrite(2, HIGH);
       }
-      //Logica para despressionar tecla e evitar repetição
     } else {
-      if (teclaPressionada[i]) { 
+      if (teclaPressionada[i]) {
         teclaPressionada[i] = false;
         bleKeyboard.release(teclas[i][0]);
         digitalWrite(2, LOW);
       }
     }
   }
-  //=====================================================
+}
+
+void connectToWiFi() {
+  Serial.println("Conectando ao WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando...");
+  }
+
+  Serial.println("Conectado ao WiFi");
 }
